71f9167023f9fb57b8b5fd424ccc518208614317
Alex Forbes <alforbes@gumtree.com>
2015-11-20 14:54:02 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import multiprocessing  # nopep8
 
 setup(
     name='orloclient',
-    version='0.0.1-4',
+    version='0.0.2',
     description='Client to the Orlo deployment data capture API',
     author='Alex Forbes',
     author_email='alforbes@ebay.com',
